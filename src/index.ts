47a98cf3ad236ab5c447ee55d626f11319670f75
Eric Bouillet <eric.bouillet@epfl.ch>
2018-12-01 17:25:30 +0100

    Use IFrame

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import {
   Message
 } from '@phosphor/messaging';
 
-import {
-  Widget
-} from '@phosphor/widgets';
+//import {
+//Widget
+//} from '@phosphor/widgets';
 
 import {
-  ICommandPalette, InstanceTracker, IInstanceTracker
+  ICommandPalette, InstanceTracker, IInstanceTracker, IFrame
 } from '@jupyterlab/apputils';
 
 import {
@@ -35,30 +35,22 @@ import '../style/index.css';
 /**
  * A VNC widget
  */
-class X11vncWidget extends Widget {
-  constructor() {
-    super();
-
-    this.id = 'jupyterlab-vnc';
-    this.title.label = 'Desktop';
-    this.title.closable = true ;
-    this.addClass('jp-vncWidget'); 
-
-    this.vncIFrame = document.createElement('iframe');
-    //this.vncIFrame.src=process.env.JUPYTERHUB_SERVICE_PREFIX + '/proxy/6080/vnc_lite.html?path=' + process.env.JUPYTERHUB_SERVICE_PREFIX;
-    this.vncIFrame.src='https://epfl.ch';
-    this.vncIFrame.className = 'jp-vncIFrame' ;
-    this.node.appendChild(this.vncIFrame);
-  } 
-
-  readonly vncIFrame : HTMLIFrameElement;
+class X11vncWidget extends IFrame {
+  constructor(url: string) {
+     super();
+     this.id = 'jupyterlab-vnc';
+     this.title.label = 'Desktop';
+     this.title.closable = true;
+     this.url = url;
+     this.addClass('jp-vncWidget'); 
+  }
 
   onUpdateRequest(msg : Message) {
   }
 }
 
 interface IX11vncTracker extends IInstanceTracker<X11vncWidget> {};
-export const IX11vncTracker = new Token<IX11vncTracker>('x11vnc/tracker');
+export const IX11vncTracker = new Token<IX11vncTracker>('@renku/x11vnc:Widget');
 
 /**
  * Acticate X11 VNC Widget
@@ -80,7 +72,7 @@ function activate(
       label: 'Open Desktop',
       execute: () => {
         if (!x11vncWidget) {
-          x11vncWidget = new X11vncWidget();
+          x11vncWidget = new X11vncWidget(Private.getUrl());
           x11vncWidget.update();
         }
         if (!tracker.has(x11vncWidget)) {
@@ -139,7 +131,19 @@ const extension: JupyterLabPlugin<IX11vncTracker> = {
   optional: [ILauncher],
   provides: IX11vncTracker,
   activate: activate
-};
+}
+
+namespace Private {
+   let VNC_URL : string;
+
+   export function getUrl() : string {
+     if (!VNC_URL) {
+       //VNC_URL=src=process.env.JUPYTERHUB_SERVICE_PREFIX + '/proxy/6080/vnc_lite.html?path=' + process.env.JUPYTERHUB_SERVICE_PREFIX;
+       VNC_URL='https://www.epfl.ch';
+     }
+     return VNC_URL;
+   }
+}
 
 export default extension;
 
