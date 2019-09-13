807efd35f6956ba9df56ceffcd97e4b614ae68fd
Eric Bouillet <eric.bouillet@epfl.ch>
2019-09-13 17:32:29 +0200

    fix hardcoded url

--- docker/start-supervisord.sh
+++ docker/start-supervisord.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 main() {
-    #set -euC
-    set -x
+    set -eux
     local -r JUPYTERLAB_DIR=$(jupyter lab path|sed -En 's/Application directory: *(.*)/\1/p')
-    local -r PLUGIN_CONFIG="${JUPYTERLAB_DIR}/schemas/@renku/jupyterlab-vnc/plugin.json"
-    local -r VNC_URL="${JUPYTERHUB_SERVICE_PREFIX:-FIXME}/proxy/6080/vnc_lite.html?path=${JUPYTERHUB_SERVICE_PREFIX:-FIXME}/proxy/6080/"
-    if [[ -e "${PLUGIN_CONFIG}" ]]; then
-        sudo sed -E -i.bak 's/https:..datascience.ch/'"${VNC_URL//\//\\\/}"'/g' "${PLUGIN_CONFIG}"
-    else
-        echo "${PLUGIN_CONFIG} -- ${VNC_URL}" > /tmp/log_VNC_URL.txt
+    local -r PLUGIN_CONFIG="${JUPYTERLAB_DIR:-}/schemas/@renku/jupyterlab-vnc/plugin.json"
+    if [[ -e "${PLUGIN_CONFIG:-}" && -n "${JUPYTERHUB_SERVICE_PREFIX:-}" ]]; then
+        local -r VNC_URL="${JUPYTERHUB_SERVICE_PREFIX}/proxy/6080/vnc_lite.html?path=${JUPYTERHUB_SERVICE_PREFIX}/proxy/6080/"
+        sudo sed -E -i.bak 's|"\$VNC_URL"|'"${VNC_URL}"'|g' "${PLUGIN_CONFIG}"
     fi
     sudo start-stop-daemon --start --quiet --startas /usr/bin/supervisord --pidfile /var/run/supervisord.pid -- -c /etc/supervisor/supervisord.conf
 }
